f1d99939c21f5ff2ea86e5b0be353ce54715928a
xww <1271701079@qq.com>
2022-05-19 19:18:19 +0800

    测试

--- router/system/user.go
+++ router/system/user.go
@@ -10,7 +10,7 @@ type Userrouter struct {
 
 func (u *Userrouter) Inituserrouter(Router *gin.RouterGroup) {
 	Api := v1.ApiGroupApp.Systemapi.Verifyapi
-	user := Router.Group("user")
+	user := Router.Group("/user")
 	{
 		user.POST("/register", Api.Register)
 	}
